c6b6eb282a7bf45b7cb93e833252970d0a79d076
MichalMroz06 <michalmroz2006@gmail.com>
2025-10-29 11:09:38 +0100

    Creating classes based on requirements

--- Program.cs
+++ Program.cs
@@ -2,11 +2,59 @@
 
 namespace UrzadzeniaDomowe
 {
-    internal class Program
+    public class Narzedzia
+    {
+        public void wyswietlKomunikat(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+
+    public class Pralka : Narzedzia
+    {
+        private int numerPrania = 0;
+
+        public void ustawNumerPrania(int value)
+        {
+            if (value >= 1 && value <= 12)
+            {
+                numerPrania = value;
+            }
+            else
+            {
+                numerPrania = 0;
+            }
+        }
+    }
+
+    public class Odkurzacz : Narzedzia
+    {
+        private bool stanOdkurzacza = false;
+
+        public void on()
+        {
+            if (stanOdkurzacza == false)
+            {
+                stanOdkurzacza = true;
+                wyswietlKomunikat("Odkurzacz włączony");
+            }
+        }
+
+        public void off()
+        {
+            if (stanOdkurzacza == true)
+            {
+                stanOdkurzacza = false;
+                wyswietlKomunikat("Odkurzacz wyłączono");
+            }
+        }
+    }
+
+internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
